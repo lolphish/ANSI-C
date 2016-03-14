d2c732221f9e7786d8f78f31711d94e7436ce387
Phi Nguyen <philn1@uci.edu>
2016-03-13 19:19:54 -0700

    Update my_ls.h

--- my_ls.h
+++ my_ls.h
@@ -9,37 +9,37 @@
 bool _strcmp_equals(char * str1, char * str2);
 
 /*
-    Check if the given directory is a valid path. If it is not, then exit the
-    program.
-*/
+ * Check if the given directory is a valid path. If it is not, then exit the
+ * program.
+ */
 DIR * open_dir(char * dirName);
 
 /*  
-    Creates a directory file, a dirent struct, a stat struct, 
-    and a char for usage in interpret file.
-*/
+ * Creates a directory file, a dirent struct, a stat struct, 
+ * and a char for usage in interpret file.
+ */
 void generate_stat(char * name, int padding);
 
 /*
-    Will take in the members amde in the generate stat function for use, then, if the
-    file or directory given is not a pointer to the current or previous directory, the
-    function will run read stat.
-*/
+ * Will take in the members amde in the generate stat function for use, then, if the
+ * file or directory given is not a pointer to the current or previous directory, the
+ * function will run read stat.
+ */
 void interpret_file(char * name, struct dirent * file, struct stat st, char * buf, int padding);
 
 /*
-	the function will print the necessary information for the file, or recursively loop 
-    if given a directory.
-*/
+ * the function will print the necessary information for the file, or recursively loop 
+ * if given a directory.
+ */
 void read_stat(char * buf, struct stat st, char * file_name, int padding);
 
 // In the beginning, check if the type of arg is file or directory, and perform their respective code
 void check_type(char * name, int padding);
 
 /*
-    prints the necessary components for a directory, then recursively
-    calls the generate_stat function for that file 
-*/
+ * prints the necessary components for a directory, then recursively
+ * calls the generate_stat function for that file 
+ */
 void _is_dir(char * name, struct stat st, char * file_name, int padding);
 
 // Print, in order: mode  num_links  owner  group  size_in_bytes modification_timedate name
